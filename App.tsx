fef466b4b4942fdee4c4c3d0be3ad53ad81362ff
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-11-11 18:31:30 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Home } from "@/app/Home";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Home />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
