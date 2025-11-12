ede277ec55a74ff5d5477f99eba34960b69486fa
Jefferson Oliveira <oliveira2112jj@outlook.com>
2025-11-12 20:28:12 -0300

    create main header component

--- src/app/Budget.tsx
+++ src/app/Budget.tsx
@@ -1,12 +1,20 @@
 import { StackRoutesProps } from "@/routes/StackRoutes";
-import { Button, Text, View } from "react-native";
+import { colors } from "@/theme";
+import { Button, StyleSheet, Text, View } from "react-native";
 
 export function Budget({ navigation }: StackRoutesProps<"budget">) {
   return (
-    <View>
+    <View style={styles.container}>
       <Text>Budget Screen</Text>
 
       <Button title="Home" onPress={() => navigation.goBack()} />
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    borderTopWidth: 1,
+    borderTopColor: colors.gray[300],
+  },
+});
